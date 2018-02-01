5aff4e33ee49cee656a3840e7878558dd5e4f43b
sayhellotogithub <wang.jun16@163.com>
2018-02-01 11:57:57 +0800

    Signed-off-by: sayhellotogithub <wang.jun16@163.com>

---
+++ app/src/main/java/com/iblogstreet/verticalseekbar/SelfVerticalSeekBar.java
@@ -0,0 +1,134 @@
+package com.iblogstreet.verticalseekbar;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.SeekBar;
+
+public class SelfVerticalSeekBar extends SeekBar {
+    private Drawable mThumb;
+    private OnSeekBarChangeListener mOnSeekBarChangeListener;
+
+    public SelfVerticalSeekBar(Context context) {
+        this(context,null);
+    }
+
+    public SelfVerticalSeekBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SelfVerticalSeekBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setOnSeekBarChangeListener(OnSeekBarChangeListener l) {
+        mOnSeekBarChangeListener = l;
+    }
+
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(h, w, oldh, oldw);
+    }
+
+    @Override
+    protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(heightMeasureSpec, widthMeasureSpec);
+        setMeasuredDimension(getMeasuredHeight(), getMeasuredWidth());
+    }
+
+    protected void onDraw(Canvas c) {
+        c.rotate(-90);
+        c.translate(-getHeight(), 0);
+
+        super.onDraw(c);
+    }
+    public void onProgressRefresh(float scale, boolean fromUser) {
+        Drawable thumb = mThumb;
+        if (thumb != null) {
+            setThumbPos(getHeight(), thumb, scale, Integer.MIN_VALUE);
+            invalidate();
+        }
+        if (mOnSeekBarChangeListener != null) {
+            mOnSeekBarChangeListener.onProgressChanged(this, getProgress(), fromUser);
+        }
+    }
+
+    private void setThumbPos(int w, Drawable thumb, float scale, int gap) {
+        int available = w - getPaddingLeft() - getPaddingRight();
+
+        int thumbWidth = thumb.getIntrinsicWidth();
+        int thumbHeight = thumb.getIntrinsicHeight();
+
+        int thumbPos = (int) (scale * available + 0.5f);
+
+        // int topBound = getWidth() / 2 - thumbHeight / 2 - getPaddingTop();
+        // int bottomBound = getWidth() / 2 + thumbHeight / 2 - getPaddingTop();
+        int topBound, bottomBound;
+        if (gap == Integer.MIN_VALUE) {
+            Rect oldBounds = thumb.getBounds();
+            topBound = oldBounds.top;
+            bottomBound = oldBounds.bottom;
+        } else {
+            topBound = gap;
+            bottomBound = gap + thumbHeight;
+        }
+        thumb.setBounds(thumbPos, topBound, thumbPos + thumbWidth, bottomBound);
+    }
+
+    public void setThumb(Drawable thumb) {
+        mThumb = thumb;
+        super.setThumb(thumb);
+    }
+
+    void onStartTrackingTouch() {
+        if (mOnSeekBarChangeListener != null) {
+            mOnSeekBarChangeListener.onStartTrackingTouch(this);
+        }
+    }
+
+    void onStopTrackingTouch() {
+        if (mOnSeekBarChangeListener != null) {
+            mOnSeekBarChangeListener.onStopTrackingTouch(this);
+        }
+    }
+
+    private void attemptClaimDrag() {
+        if (getParent() != null) {
+            getParent().requestDisallowInterceptTouchEvent(true);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!isEnabled()) {
+            return false;
+        }
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                setPressed(true);
+                onStartTrackingTouch();
+                onSizeChanged(getWidth(), getHeight(), 0, 0);
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                attemptClaimDrag();
+                setProgress(getMax() - (int) (getMax() * event.getY() / getHeight()));
+                onSizeChanged(getWidth(), getHeight(), 0, 0);
+                break;
+            case MotionEvent.ACTION_UP:
+                onStopTrackingTouch();
+                setPressed(false);
+                onSizeChanged(getWidth(), getHeight(), 0, 0);
+                break;
+
+            case MotionEvent.ACTION_CANCEL:
+                onStopTrackingTouch();
+                setPressed(false);
+                break;
+        }
+        return true;
+    }
+}
